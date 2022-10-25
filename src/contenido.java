ed9f871ad90b649c7ed4e6587ac05cac9c211640
Juan20040809 <89270505+Juan20040809@users.noreply.github.com>
2022-10-25 13:00:31 -0500

    Trabajo de programacion

---
+++ src/contenido.java
@@ -0,0 +1,35 @@
+public class contenido {
+    public static void main(String[] args) {
+
+
+        System.out.println("Un software es un conjunto de instrucciones que permiten ordenar a una computadora el \n" +
+                "proceso para resolver un problema.");
+        System.out.println("Un algoritmo es un método que describe la solución de un problema a través de un computador, \n" +
+                "mediante una serie de pasos precisos, definidos y finitos.");
+        System.out.println("Las caracteristicas de un algoritmo es que es: ordenado, finito y definido.");
+        System.out.println("Programación es la acción de escribir instrucciones y crear programas.");
+        System.out.println("El sistema binario, es un sistema de numeración natural de las computadoras");
+        System.out.println("El sistema decimal, es un sistema de numeración natural de los seres \n" +
+                "humanos.");
+        System.out.println("La Programación da vida al hardware.");
+        System.out.println("Un programa se escribe en un lenguaje de programación.");
+        System.out.println("Las instrucciones que expresan un algoritmo, se llama \n" +
+                "programación.");
+        System.out.println("Un entorno de programacion es un programa o programas que facilita al programador \n" +
+                "el desarrollo de programas.");
+        System.out.println("los lenguajes mas usados para escribir programas de \n" +
+                "computadora son java, c++ y python.");
+        System.out.println("Las instrucciones que expresan un algoritmo, se llama \n" +
+                "programación.");
+        System.out.println("Bit es el elemento mas pequeño en una computadora.");
+        System.out.println("Byte es el conjunto de 8 bits.");
+        System.out.println("La programacion ayuda a comunicar instrucciones a los dispositivos.");
+        System.out.println("Un algoritmo tiene 3 partes: Entrada, procesos y salidas.");
+        System.out.println("Para realizar un algoritmo, se debe realizar un analisis, un diseño y pruebas.");
+        System.out.println("Lógica es el razonamiento de ideas de forma coherente.");
+        System.out.println("Las formas más comunes en que representamos los algoritmos son \n" +
+                "a través de pseudocódigo y diagramas de flujo.");
+        System.out.println("Primero se crea el algoritmo y luego se programa.");
+    }
+
+    }
